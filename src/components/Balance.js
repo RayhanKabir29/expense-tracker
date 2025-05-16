2886d587034d65b36d2eca0fe0e595d2fbcbfbac
RayhanKabir29 <rayhan.kabir29@gmail.com>
2025-05-16 11:29:25 +0600

    balance calculation

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,12 +1,27 @@
-import React from "react";
+import { useSelector } from "react-redux";
 
 const Balance = () => {
+  const {transactions} = useSelector((state)=>state.transaction)
+  const calculateIncom =()=>{
+    let balance = 0;
+    transactions?.forEach(transaction => {
+      const {type,amount} = transaction;
+      if(type === 'income'){
+        balance +=amount
+      }
+      else{
+        balance -=amount
+      }
+    });
+    return balance;
+  }
+
   return (
     <div className="top_card">
       <p>Your Current Balance</p>
       <h3>
         <span>৳</span>
-        <span>10500</span>
+       { transactions?.length > 0 ?<span>{calculateIncom(transactions)}</span>:<span>0</span>}
       </h3>
     </div>
   );
